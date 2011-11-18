a0e01183926e474b457af3f68ec8d10e62c1e243
Sander Mathijs van Veen <smvv@kompiler.org>
2011-11-18 16:06:45 +0100

    removed old generated files and update git ignore list.

--- src/pyrex/bison_.h
+++
@@ -1,3 +0,0 @@
-extern DL_IMPORT(PyObject) *(py_callback(PyObject *,char (*),int ,int ,void (*),...));
-extern DL_IMPORT(void) (py_input(PyObject *,char (*),int (*),int ));
-extern DL_IMPORT(void) initbison_(void);
