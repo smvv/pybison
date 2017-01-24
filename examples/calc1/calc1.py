4987cc7a5352d030170d2ffc1badc1465682da29
Sander Mathijs van Veen <sandervv@gmail.com>
2017-01-24 22:38:00 +0100

    Fix issue #9 (re-definition of yylineno in calc1)

--- examples/calc1/calc1.py
+++ examples/calc1/calc1.py
@@ -207,7 +207,6 @@ class Parser(BisonParser):
     # -----------------------------------------
     lexscript = r"""
     %{
-    int yylineno = 0;
     #include <stdio.h>
     #include <string.h>
     #include "Python.h"
